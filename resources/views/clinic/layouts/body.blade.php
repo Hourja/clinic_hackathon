35352414cdac0f38641de8987402c1eb2c58f0f2
Hourja <tgafalkon@gmail.com>
2021-06-13 22:53:50 +0300

    Fixed some CSS issues and removed irrelevant css file

--- resources/views/clinic/layouts/body.blade.php
+++ resources/views/clinic/layouts/body.blade.php
@@ -8,11 +8,11 @@
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,700;0,900;1,100;1,900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="{{mix('css/app.css')}}">
     <title>{{$title}}</title>
-    {{-- <style>
+    <style>
         a.current{
             text-decoration: none;
         }
-    </style> --}}
+    </style>
 </head>
 <body>
 
--- resources/views/clinic/partials/nav_bar.blade.php
+++ resources/views/clinic/partials/nav_bar.blade.php
@@ -1,10 +1,9 @@
 <header>
     <nav>
-
-        <a class="nav_link" href="{{route('home')}}" @if ($current == 'home') class="current" @endif>HOME</a>
-        <a class="nav_link" href="{{route('database-api')}}"@if ($current == 'api') class="current" @endif>API</a>
-        <a class="nav_link" href="{{route('about_us')}}"@if ($current == 'about_us') class="current" @endif>ABOUT</a>
-        <a class="nav_link" href="{{route('client-create')}}"@if ($current == 'create-client') class="current" @endif>ADMIN</a>
+        <a href="{{route('home')}}" @if ($current == 'home') class="current" @endif>HOME</a>
+        <a href="{{route('database-api')}}"@if ($current == 'api') class="current" @endif>API</a>
+        <a href="{{route('about_us')}}"@if ($current == 'about_us') class="current" @endif>ABOUT</a>
+        <a href="{{route('client-create')}}"@if ($current == 'create-client') class="current" @endif>ADMIN</a>
 
     </nav>
 
